ee13539e36a65f2693f9e67ed0b2e38ed2ca0148
Dariusch Ochlast <d.ochlast@buddyandselly.com>
2017-09-21 13:58:16 +0200

    remove comments

--- lib/plenty_client/document.rb
+++ lib/plenty_client/document.rb
@@ -1,13 +1,3 @@
-
-
-/orders/{orderId}/documents/downloads/{type?} Download documents of an order
-/orders/documents/downloads/{type} Download documents of a document type
-/orders/{orderId}/documents/{type?} List documents of an order
-/orders/documents/{type} List documents of a type
-/orders/{orderId}/documents/{type} Upload order documents
-
-/documents/{documentId} Download the content of a document
-
 module PlentyClient
   module Document
     include PlentyClient::Endpoint
