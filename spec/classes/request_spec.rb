7971f8b0615d91a0085f7834b995849722eac243
Dariusch Ochlast <d.ochlast@buddyandselly.com>
2017-10-07 21:50:29 +0200

    rename variable

--- spec/classes/request_spec.rb
+++ spec/classes/request_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe PlentyClient::Request::ClassMethods do
-  let(:ic) { Class.new { include PlentyClient::Request } }
+  let(:request_client) { Class.new { include PlentyClient::Request } }
 
   before(:each) do
     PlentyClient::Config.site_url = 'https://www.example.com'
   end
 
-  def stub_api_tokens(access_token: 'foobar', expiry_date: Time.now + 86400,
-                      refresh_token: 'foobar')
+  def stub_api_tokens(access_token: 'foobar', expiry_date: Time.now + 86400, refresh_token: 'foobar')
     PlentyClient::Config.access_token = access_token
     PlentyClient::Config.refresh_token = refresh_token
     PlentyClient::Config.expiry_date = expiry_date
@@ -24,32 +23,32 @@ RSpec.describe PlentyClient::Request::ClassMethods do
 
       context 'with valid arguments' do
         it 'makes a HTTP call' do
-          ic.request(:post, '/index.html')
+          request_client.request(:post, '/index.html')
           expect(WebMock).to have_requested(:post, /example/)
         end
       end
 
       context 'without http_method' do
         it 'returns false' do
-          response = ic.request(nil, '/index.html')
+          response = request_client.request(nil, '/index.html')
           expect(response).to be false
         end
 
         it 'does not make a HTTP call' do
           expect(WebMock).not_to have_requested(:any, /example/)
-          ic.request(nil, '/index.html')
+          request_client.request(nil, '/index.html')
         end
       end
 
       context 'without path' do
         it 'returns false' do
-          response = ic.request(:post, nil)
+          response = request_client.request(:post, nil)
           expect(response).to be false
         end
 
         it 'does not make a HTTP call' do
           expect(WebMock).not_to have_requested(:any, /example/)
-          ic.request(:post, nil)
+          request_client.request(:post, nil)
         end
       end
     end
@@ -57,29 +56,29 @@ RSpec.describe PlentyClient::Request::ClassMethods do
     describe 'wrappers for #request' do
       describe '#post' do
         it 'calls #request with :post and rest of params' do
-          expect(ic).to receive(:request).with(:post, '/index.php', 'param1' => 'value1')
-          ic.post('/index.php', 'param1' => 'value1')
+          expect(request_client).to receive(:request).with(:post, '/index.php', 'param1' => 'value1')
+          request_client.post('/index.php', 'param1' => 'value1')
         end
       end
 
       describe '#put' do
         it 'calls #request with :put and rest of params' do
-          expect(ic).to receive(:request).with(:put, '/index.php', 'param1' => 'value1')
-          ic.put('/index.php', 'param1' => 'value1')
+          expect(request_client).to receive(:request).with(:put, '/index.php', 'param1' => 'value1')
+          request_client.put('/index.php', 'param1' => 'value1')
         end
       end
 
       describe '#patch' do
         it 'calls #request with :patch and rest of params' do
-          expect(ic).to receive(:request).with(:patch, '/index.php', 'param1' => 'value1')
-          ic.patch('/index.php', 'param1' => 'value1')
+          expect(request_client).to receive(:request).with(:patch, '/index.php', 'param1' => 'value1')
+          request_client.patch('/index.php', 'param1' => 'value1')
         end
       end
 
       describe '#delete' do
         it 'calls #request with :delete and rest of params' do
-          expect(ic).to receive(:request).with(:delete, '/index.php', 'param1' => 'value1')
-          ic.delete('/index.php', 'param1' => 'value1')
+          expect(request_client).to receive(:request).with(:delete, '/index.php', 'param1' => 'value1')
+          request_client.delete('/index.php', 'param1' => 'value1')
         end
       end
 
@@ -87,15 +86,17 @@ RSpec.describe PlentyClient::Request::ClassMethods do
         context 'when called without a block' do
           context 'when called without page param' do
             it 'calls #request with :get and rest of params, merged with page: 1' do
-              expect(ic).to receive(:request).with(:get, '/index.php', 'p1' => 'v1', 'page' => 1)
-              ic.get('/index.php', 'p1' => 'v1')
+              expect(request_client).to receive(:request).with(:get, '/index.php', 'p1' => 'v1', 'page' => 1)
+              request_client.get('/index.php', 'p1' => 'v1')
             end
           end
 
           context 'when called with page param' do
             it 'calls #request with :get and unchanged params' do
-              expect(ic).to receive(:request).with(:get, '/index.php', hash_including('p1' => 'v1', 'page' => 100))
-              ic.get('/index.php', 'p1' => 'v1', 'page' => 100)
+              expect(request_client)
+                .to receive(:request)
+                .with(:get, '/index.php', hash_including('p1' => 'v1', 'page' => 100))
+              request_client.get('/index.php', 'p1' => 'v1', 'page' => 100)
             end
           end
         end
@@ -118,14 +119,14 @@ RSpec.describe PlentyClient::Request::ClassMethods do
           end
 
           it 'calls #request with get until it gets last page' do
-            ic.get('/index.php', {}) do
+            request_client.get('/index.php', {}) do
               'Hello world'
             end
             expect(WebMock).to have_requested(:get, /example/).times(3)
           end
 
           it 'yields entries n times' do
-            expect { |b| ic.get('/index.php', {}, &b) }.to yield_control.exactly(3).times
+            expect { |b| request_client.get('/index.php', {}, &b) }.to yield_control.exactly(3).times
           end
         end
       end
@@ -138,14 +139,15 @@ RSpec.describe PlentyClient::Request::ClassMethods do
         PlentyClient::Config.access_token = nil
         PlentyClient::Config.refresh_token = nil
         PlentyClient::Config.expiry_date = nil
-        @login_request = stub_request(:post, /login/).to_return(body: {
-          'tokenType' => 'Bearer',
-          'expiresIn' => 86400,
-          'accessToken' => 'foo_access_token',
-          'refreshToken' => 'foo_refresh_token'
-        }.to_json)
-        @actual_request = stub_request(:post, /index\.html/).to_return(body: {
-        }.to_json)
+        @login_request = stub_request(:post, /login/)
+                         .to_return(body: {
+                           'tokenType' => 'Bearer',
+                           'expiresIn' => 86400,
+                           'accessToken' => 'foo_access_token',
+                           'refreshToken' => 'foo_refresh_token'
+                         }.to_json)
+        @actual_request = stub_request(:post, /index\.html/)
+                          .to_return(body: {}.to_json)
       end
 
       context 'when credentials are missing' do
@@ -155,7 +157,8 @@ RSpec.describe PlentyClient::Request::ClassMethods do
         end
 
         it 'raises PlentyClient::Config::NoCredentials' do
-          expect { ic.request(:post, '/index.html') }.to raise_exception(PlentyClient::Config::NoCredentials)
+          expect { request_client.request(:post, '/index.html') }
+            .to raise_exception(PlentyClient::Config::NoCredentials)
         end
 
         it 'does not perform login request' do
@@ -174,13 +177,13 @@ RSpec.describe PlentyClient::Request::ClassMethods do
         end
 
         it 'performs a POST request with username and password' do
-          ic.request(:post, '/index.html')
+          request_client.request(:post, '/index.html')
           expect(@login_request).to have_been_made.once
         end
 
         context 'when credentials are correct' do
           before(:each) do
-            ic.request(:post, '/index.html')
+            request_client.request(:post, '/index.html')
           end
 
           it 'sets Config.access_token' do
@@ -213,13 +216,14 @@ RSpec.describe PlentyClient::Request::ClassMethods do
           end
 
           it 'raises PlentyClient::Config::InvalidCredentials' do
-            expect { ic.request(:post, '/index.html') }.to raise_exception(PlentyClient::Config::InvalidCredentials)
+            expect { request_client.request(:post, '/index.html') }
+              .to raise_exception(PlentyClient::Config::InvalidCredentials)
           end
 
           describe 'handling' do
             before(:each) do
               begin
-                ic.request(:post, '/index.html') 
+                request_client.request(:post, '/index.html')
               rescue PlentyClient::Config::InvalidCredentials
               end
             end
@@ -262,7 +266,7 @@ RSpec.describe PlentyClient::Request::ClassMethods do
     context 'when expiry_date is in the past' do
       it 'sends login request' do
         PlentyClient::Config.expiry_date = Time.now - 600
-        ic.request(:post, '/index.html')
+        request_client.request(:post, '/index.html')
         expect(@login_request).to have_been_made.once
       end
     end
@@ -270,7 +274,7 @@ RSpec.describe PlentyClient::Request::ClassMethods do
     context 'when expiry_date is <60s from now' do
       it 'sends login request' do
         PlentyClient::Config.expiry_date = Time.now + 45
-        ic.request(:post, '/index.html')
+        request_client.request(:post, '/index.html')
         expect(@login_request).to have_been_made.once
       end
     end
@@ -278,7 +282,7 @@ RSpec.describe PlentyClient::Request::ClassMethods do
     context 'when expiry_date is >60s from now' do
       it 'does not send login request' do
         PlentyClient::Config.expiry_date = Time.now + 600
-        ic.request(:post, '/index.html')
+        request_client.request(:post, '/index.html')
         expect(@login_request).not_to have_been_made
       end
     end
