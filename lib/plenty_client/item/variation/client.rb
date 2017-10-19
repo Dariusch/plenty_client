d8ff9d158a8ad054c75df26095c8283b49307531
Dariusch Ochlast <d.ochlast@buddyandselly.com>
2017-10-19 09:01:28 +0200

    update variation client route

--- lib/plenty_client/item/variation/client.rb
+++ lib/plenty_client/item/variation/client.rb
@@ -24,11 +24,11 @@ module PlentyClient
                                 variation: variation_id), body)
           end
 
-          def destroy(item_id, variation_id, marketplace_id)
+          def destroy(item_id, variation_id, plenty_id)
             delete(build_endpoint("#{ITEM_VARIATION_CLIENTS_PATH}#{DELETE_ITEM_VARIATIONS_CLIENTS}",
                                   item: item_id,
                                   variation: variation_id,
-                                  marketplace: marketplace_id))
+                                  plenty_id: plenty_id))
           end
         end
       end
