93383d4ab89c4ecf7ca6c3ce4b377c1e52499de0
Dariusch Ochlast <d.ochlast@buddyandselly.com>
2017-11-17 13:44:43 +0100

    refactor: Warehouse::Management methods

--- lib/plenty_client/warehouse/management.rb
+++ lib/plenty_client/warehouse/management.rb
@@ -7,18 +7,32 @@ module PlentyClient
       WM_STORAGE_LOCATION_BASE_PATH = '/stockmanagement/warehouses/{warehouseId}/management'.freeze
       WM_LIST_STORAGE_LOCATIONS     = '/storageLocations'.freeze
       WM_FIND_STORAGE_LOCATIONS     = '/storageLocations/{storageLocationId}'.freeze
+      WM_LIST_RACKS                 = '/racks'.freeze
+      WM_FIND_RACKS                 = '/racks/{rackId}'.freeze
 
       class << self
-        def list(warehouse_id, headers = {}, &block)
+        def list_locations(warehouse_id, headers = {}, &block)
           get(build_endpoint("#{WM_STORAGE_LOCATION_BASE_PATH}#{WM_LIST_STORAGE_LOCATIONS}",
                              warehouse: warehouse_id),
               headers, &block)
         end
 
-        def find(warehouse_id, storage_location_id, headers = {}, &block)
+        def find_location(warehouse_id, storage_location_id, headers = {}, &block)
           get(build_endpoint("#{WM_STORAGE_LOCATION_BASE_PATH}#{WM_FIND_STORAGE_LOCATIONS}",
                              warehouse: warehouse_id,
                              storage_location: storage_location_id),
+              headers, &block)
+        end
+
+        def list_racks(warehouse_id, headers = {}, &block)
+          get(build_endpoint("#{WM_STORAGE_LOCATION_BASE_PATH}#{WM_LIST_RACKS}",
+                             warehouse: warehouse_id),
+              headers, &block)
+        end
+
+        def find_racks(warehouse_id, headers = {}, &block)
+          get(build_endpoint("#{WM_STORAGE_LOCATION_BASE_PATH}#{WM_FIND_RACKS}",
+                             warehouse: warehouse_id),
               headers, &block)
         end
       end
