7435b89bab67a42b2d53d726beb31e769641bcb5
Karol M <dmuhafc@gmail.com>
2018-01-05 10:36:32 +0100

    Fix VariationProperty paths again

--- lib/plenty_client/item/variation/property.rb
+++ lib/plenty_client/item/variation/property.rb
@@ -7,20 +7,29 @@ module PlentyClient
         include PlentyClient::Endpoint
         include PlentyClient::Request
 
-        VARIATION_PROPERTY_BASE_PATH  = '/items/{itemId}/variations/{variationId}/variation_properties'
-        SINGLE_PROPERTY_PATH          = VARIATION_PROPERTY_BASE_PATH + '/{propertyId}'
+        BASE_PATH            = '/items/{itemId}/variations/{variationId}/variation_properties'
+        SINGLE_PROPERTY_PATH = BASE_PATH + '/{propertyId}'
+        BULK_ASSIGNMENT      = '/items/variations/variation_properties'
 
         class << self
           def list(item_id, variation_id, headers = {}, &block)
-            get(build_endpoint(VARIATION_PROPERTY_BASE_PATH, item: item_id, variation: variation_id), headers, &block)
+            get(build_endpoint(BASE_PATH, item: item_id, variation: variation_id), headers, &block)
           end
 
           def create(item_id, variation_id, headers = {})
-            post(build_endpoint(VARIATION_PROPERTY_BASE_PATH, item: item_id, variation: variation_id), headers)
+            post(build_endpoint(BASE_PATH, item: item_id, variation: variation_id), headers)
           end
 
           def destroy_all(item_id, variation_id)
-            delete(build_endpoint(VARIATION_PROPERTY_BASE_PATH, item: item_id, variation: variation_id))
+            delete(build_endpoint(BASE_PATH, item: item_id, variation: variation_id))
+          end
+
+          def bulk_create(headers = {})
+            post(BULK_ASSIGNMENT, headers)
+          end
+
+          def bulk_update(headers = {})
+            put(BULK_ASSIGNMENT, headers)
           end
 
           def find(item_id, variation_id, property_id, headers = {}, &block)
