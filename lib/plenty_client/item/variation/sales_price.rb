a2becf4aba3be82daf91c88a5a524b5874df1630
Karol M <dmuhafc@gmail.com>
2018-01-10 12:07:20 +0100

    Add route for mass update of SalesPrices

--- lib/plenty_client/item/variation/sales_price.rb
+++ lib/plenty_client/item/variation/sales_price.rb
@@ -7,42 +7,36 @@ module PlentyClient
         include PlentyClient::Endpoint
         include PlentyClient::Request
 
-        ITEM_VARIATION_SALES_PRICES_PATH    = '/items/{itemId}/variations/{variationId}'
-
-        LIST_ITEM_VARIATIONS_SALES_PRICES   = '/variation_sales_prices'
-        GET_ITEM_VARIATIONS_SALES_PRICES    = '/variation_sales_prices/{priceId}'
-        CREATE_ITEM_VARIATIONS_SALES_PRICES = '/variation_sales_prices'
-        UPDATE_ITEM_VARIATIONS_SALES_PRICES = '/variation_sales_prices/{priceId}'
-        DELETE_ITEM_VARIATIONS_SALES_PRICES = '/variation_sales_prices/{priceId}'
+        BASE_PATH     = '/items/{itemId}/variations/{variationId}'
+        PLURAL_PATH   = BASE_PATH + '/variation_sales_prices'
+        SINGULAR_PATH = PLURAL_PATH + '/{priceId}'
+        BULK_PATH     = '/items/variations/variation_sales_prices'
 
         class << self
           def list(item_id, variation_id, headers = {}, &block)
-            get(build_endpoint("#{ITEM_VARIATION_SALES_PRICES_PATH}#{LIST_ITEM_VARIATIONS_SALES_PRICES}",
-                               item: item_id, variation: variation_id),
+            get(build_endpoint(PLURAL_PATH, item: item_id, variation: variation_id),
                 headers, &block)
           end
 
           def find(item_id, variation_id, price_id, headers = {}, &block)
-            get(build_endpoint("#{ITEM_VARIATION_SALES_PRICES_PATH}#{GET_ITEM_VARIATIONS_SALES_PRICES}",
-                               item: item_id, variation: variation_id, price: price_id),
+            get(build_endpoint(SINGULAR_PATH, item: item_id, variation: variation_id, price: price_id),
                 headers, &block)
           end
 
           def create(item_id, variation_id, body = {})
-            post(build_endpoint("#{ITEM_VARIATION_SALES_PRICES_PATH}#{CREATE_ITEM_VARIATIONS_SALES_PRICES}",
-                                item: item_id, variation: variation_id),
-                 body)
+            post(build_endpoint(PLURAL_PATH, item: item_id, variation: variation_id), body)
           end
 
           def update(item_id, variation_id, price_id, body = {})
-            put(build_endpoint("#{ITEM_VARIATION_SALES_PRICES_PATH}#{UPDATE_ITEM_VARIATIONS_SALES_PRICES}",
-                               item: item_id, variation: variation_id, price: price_id),
-                body)
+            put(build_endpoint(SINGULAR_PATH, item: item_id, variation: variation_id, price: price_id), body)
           end
 
           def destroy(item_id, variation_id, price_id)
-            delete(build_endpoint("#{ITEM_VARIATION_SALES_PRICES_PATH}#{DELETE_ITEM_VARIATIONS_SALES_PRICES}",
-                                  item: item_id, variation: variation_id, price: price_id))
+            delete(build_endpoint(SINGULAR_PATH, item: item_id, variation: variation_id, price: price_id))
+          end
+
+          def bulk_update(body = {})
+            put(BULK_PATH, body)
           end
         end
       end
