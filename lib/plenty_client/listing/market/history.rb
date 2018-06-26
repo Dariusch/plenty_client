73f5ff1ed7a9623c8bc669225eda22879cdcba56
Dariusch Ochlast <d.ochlast@buddyandselly.com>
2018-06-26 10:07:11 +0200

    feat: update myna market histories

--- lib/plenty_client/listing/market/history.rb
+++ lib/plenty_client/listing/market/history.rb
@@ -7,11 +7,12 @@ module PlentyClient
         include PlentyClient::Endpoint
         include PlentyClient::Request
 
-        FIND_LISTING_MARKET_HISTORY   = '/listings/markets/histories/{marketListingHistoryId}'
-        LIST_LISTING_MARKET_HISTORIES = '/listings/markets/histories'
-        END_LISTING_MARKET_HISTORY    = '/listings/markets/histories/end/{marketListingHistoryId}'
-        RELIST_LISTING_MARKET_HISTORY = '/listings/markets/histories/relist/{marketListingHistoryId}'
-        UPDATE_LISTING_MARKET_HISTORY = '/listings/markets/histories/update/{marketListingHistoryId}'
+        FIND_LISTING_MARKET_HISTORY     = '/listings/markets/histories/{marketListingHistoryId}'
+        LIST_LISTING_MARKET_HISTORIES   = '/listings/markets/histories'
+        END_LISTING_MARKET_HISTORY      = '/listings/markets/histories/end/{marketListingHistoryId}'
+        RELIST_LISTING_MARKET_HISTORY   = '/listings/markets/histories/relist/{marketListingHistoryId}'
+        UPDATE_LISTING_MARKET_HISTORY   = '/listings/markets/histories/update/{marketListingHistoryId}'
+        UPDATE_LISTING_MARKET_HISTORIES = '/listings/markets/histories/update'
 
         class << self
           def find(market_listing_history_id, headers = {}, &block)
@@ -33,6 +34,10 @@ module PlentyClient
 
           def update(market_listing_history_id, body = {})
             put(build_endpoint(UPDATE_LISTING_MARKET_HISTORY, market_listing_history: market_listing_history_id), body)
+          end
+
+          def update_many(body = {})
+            put(build_endpoint(UPDATE_LISTING_MARKET_HISTORIES), body)
           end
         end
       end
